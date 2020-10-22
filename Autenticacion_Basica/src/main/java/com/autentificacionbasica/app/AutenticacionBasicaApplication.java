a265192f22d101f32d4e3161e096181a4f2fcfaf
Cesar <cesarvargasescorcia@gmail.com>
2020-10-22 17:36:58 -0500

    Inicializando proyecto - properties - entity Profesor

---
+++ Autenticacion_Basica/src/main/java/com/autentificacionbasica/app/AutenticacionBasicaApplication.java
@@ -0,0 +1,13 @@
+package com.autentificacionbasica.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticacionBasicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticacionBasicaApplication.class, args);
+	}
+
+}
---
+++ Autenticacion_Basica/src/main/java/com/autentificacionbasica/app/entity/Profesor.java
@@ -0,0 +1,19 @@
+package com.autentificacionbasica.app.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name= "profesor")
+public class Profesor  {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nombre;
+}
